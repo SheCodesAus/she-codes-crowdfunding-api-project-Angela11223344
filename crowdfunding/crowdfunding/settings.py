cb4a2465b2e00a2c19acc2ba2c914c7e05f560d1
Angela11223344 <113986268+Angela11223344@users.noreply.github.com>
2023-03-08 15:18:06 +0800

    Fixed the settings error

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -144,10 +144,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     	'rest_framework.authentication.SessionAuthentication',
-    ]
-}
+    ],
 
-REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter'
